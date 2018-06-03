053b5e63a5f34f0ce3de85a023a4f2554342e27f
reiji <31179220+reud@users.noreply.github.com>
2018-06-03 19:28:37 +0900

    0.06

---
+++ KeyBoardSwitcherByConsole/KeyEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyBoardSwitcherByConsole
+{
+    class KeyEvent
+    {
+        List<string> keyList = new List<string>();
+        public KeyEvent(string path)
+        {
+            StreamReader streamreader = new StreamReader(path, Encoding.GetEncoding("Shift_JIS"));
+            string tmp=streamreader.ReadLine();
+            while(!(tmp.Equals("")))
+            {
+                keyList.Add(tmp);
+            }
+        }
+    }
+}
--- KeyBoardSwitcherByConsole/Writer.cs
+++ KeyBoardSwitcherByConsole/Writer.cs
@@ -15,9 +15,10 @@ namespace KeyBoardSwitcherByConsole
         {
             this.path = path;
             System.Diagnostics.Process.Start(path);
-            Thread.Sleep(10000);
+            Thread.Sleep(20000);
             //GetBlank();
-            HeloWorld();
+            //HeloWorld();
+            Write();
         }
         void GetBlank()
         {
@@ -34,16 +35,39 @@ namespace KeyBoardSwitcherByConsole
         }
         void HeloWorld()
         {
-            SenE("void setup(){{}");
+            SenE("void setup{(}{)}{{}");
             SenE("Serial.begin{(}9600{)};");
             SenE("{}}");
-            SenE("void loop(){{}");
+            SenE("void loop{(}{)}{{}");
             SenE("Serial.println{(}\"Hello, World\"{)};");
             //SenE("{}}");
+        }
+        void Write()//実際はProcedure pを引数にとってほしい}
 
+        {
+            SenE("#include <Digikeyboard.h>");
+            SenE("");//この辺でDefineをしとく
+            SenE("const int key1 = 1;");
+            SenE("const int key2 = 0;");
+            SenE("const int key3 = 2;");
+            SenE("bool state = false;");
+            SenE("void setup{(}{)} {{}");
+            SenE("pinMode{(}key1,INPUT{)};");
+            SenE("pinMode{(}key2,INPUT{)};");
+            SenE("pinMode{(}key3,INPUT{)};");
+            SenE("{}}");
+            SenE("void loop{(}{)}{{}");
+            SenE("if{(}digitalRead{(}Key1{)} == LOW && digitalRead{(}Key2{)} == LOW && digitalRead{(}Key3{)} == LOW {{}");
+            SenE("state = false;");
+            //ここから下に機能を書く
 
-
-
+            //機能を書くのはここまで
+            SenE("else {{}");
+            SenE("state = true;");
+            SenE("{}}");
+            SenE("DigiKeyboard.delay{(}10{)};");
+            SenE("//2018.04.08 Firmware for Extension Keyboard powered by DigiSpark USB Development Board");
+            SenE("//Written by Atsushi Kambayashi All Rights Reserved.");
         }
         void SenE(string s)
         {
